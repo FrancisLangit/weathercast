253dcc499de06ae4180a9e85c3584287c57214b7
Francis Langit <francis.villanueva.langit@gmail.com>
2021-06-06 17:44:36 +0800

    Construct rudimentary form for city searching
    Current version logs JSON of weather data to console.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-const openWeatherApi = (() => {
-    /**Methods for getting promises from Open Weather API.*/
+import './styles/style.css';
 
-    let _apiKey = '5fa4d007560f37c7fe7dc7244617ef63';
+import { openWeatherApi } from './scripts/openWeatherApi.js';
 
-    let getPromise = (cityName) => {
-        /**
-         * Get promise from API holding weather data of a city. 
-         * 
-         * Returns promise from Open Weather API with value as JSON object
-         * holding weather data of city requested.
-         * 
-         * @param {string} cityName Name of city to get data for.
-         * 
-         * @return {Promise} Promise holding weather data of city as JSON.
-        */
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${
-            cityName}&APPID=${_apiKey}`;
-        return fetch(url, {mode: 'cors'})
-            .then(response => response.json());
-            
-    }
+let searchCityForm = document.getElementById('searchCityForm');
+searchCityForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-    return { getPromise }
-})();
-
-openWeatherApi.getPromise('boston')
-    .then(data => {
-        console.log(data)
-        console.log(data.clouds.all)
-    });
+    let searchCityInput = document.getElementById('searchCityInput').value;
+    openWeatherApi.getPromise(searchCityInput)
+        .then(data => console.log(data));
+});

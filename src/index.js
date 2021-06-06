567262c99f4358b8544b7d60c1b2a5f85f44e543
Francis Langit <francis.villanueva.langit@gmail.com>
2021-06-06 15:07:41 +0800

    Create code for testing out API calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import './style.css';
-import rickAstley from './assets/images/rickAstley.jpg';
+let location = 'manila';
+let apiKey = '5fa4d007560f37c7fe7dc7244617ef63';
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&` +
+    `APPID=${apiKey}`
 
-const rickAstleyImage = new Image();
-rickAstleyImage.src = rickAstley;
-
-document.body.appendChild(rickAstleyImage);
+fetch(apiUrl, {mode: 'cors'})
+    .then(function(response) {
+        return (response.json());
+    })
+    .then(function(response) {
+        console.log(response);
+    });

69dfab2c4aea2b53492633f61393db60ce2659bf
Francis Langit <francis.villanueva.langit@gmail.com>
2021-06-06 17:58:40 +0800

    Create separate module for search city form

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import './styles/style.css';
 
 import { openWeatherApi } from './scripts/openWeatherApi.js';
 
-let searchCityForm = document.getElementById('searchCityForm');
-searchCityForm.addEventListener('submit', (event) => {
-    event.preventDefault();
 
-    let searchCityInput = document.getElementById('searchCityInput').value;
-    openWeatherApi.getPromise(searchCityInput)
-        .then(data => console.log(data));
-});
+const searchCityForm = (() => {
+    /**Form that user fills in when searching for a city.*/
+
+    let _displayCityWeatherData = () => {
+        /**
+         * Displays weather data of city queried by user in form.*/
+        let userInput = document.getElementById('searchCityInput').value;
+        openWeatherApi.getPromise(userInput)
+            .then(data => console.log(data));
+    }
+
+    let _setUp = () => {
+        /**
+         * Sets up methods to fire upon submission of form.
+         * 
+         * Adds submit event listener to form making it display data from Open
+         * Weather API based on user's inputted city.*/
+        let form = document.getElementById('searchCityForm');
+        form.addEventListener('submit', event => {
+            event.preventDefault(); // Prevent submit from refreshing browser.
+            _displayCityWeatherData();
+        });
+    }
+
+    _setUp();
+})();
